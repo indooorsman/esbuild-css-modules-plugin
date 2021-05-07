97907e012bf9c471a58c5ccae19e2af1cd4ea9db
chuanyewang <chuanyewang@freewheel.tv>
2021-05-07 15:34:19 +0800

    refine tests

---
+++ test/components/hello.world.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/app.modules.css';
+
+export const HelloWorld = () => {
+  return (
+    <h3 className={styles.helloWorld}>Hello World!</h3>
+  );
+};
