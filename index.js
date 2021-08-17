1e356b5faaae192e7c400ec24d2795537fb170fe
Benjamin Holmes <bholmesdev@gmail.com>
2021-08-17 04:40:47 -0500

    Fix: user inject function receives unprocessed CSS (#7)

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const buildCssModulesJS = async (cssFullPath, options) => {
 })();
     `;
   } else if (typeof inject === 'function') {
-    injectedCode = inject(css, digest);
+    injectedCode = inject(result.css, digest);
   }
 
   let jsContent = `
