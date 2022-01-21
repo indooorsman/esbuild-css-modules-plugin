0bd735f5b0a383a1a4bcc68bcda5cb7eaa6aae43
indooorsman <indooorsman@gmail.com>
2022-01-21 17:01:22 +0800

    support css module options (#15)

--- test/components/hello.world.jsx
+++ test/components/hello.world.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import styles, * as appCssModules from '../styles/app.modules.css';
+import styles2, * as h from '../styles/deep/styles/hello.modules.css';
 
-export const HelloWorld = () => <>
-  <h3 className={styles.helloWorld}>Hello World!</h3>
-  {
-    appCssModules.digest && <pre><code>${appCssModules.digest}</code></pre>
-  }
-  {
-    appCssModules.css && <pre><code>${appCssModules.css}</code></pre>
-  }
-</>
+export const HelloWorld = () => (
+  <>
+    <h3 className={styles.helloWorld}>Hello World!</h3>
+    <p className={styles2.helloText}>hi...</p>
+    <pre>
+      <code>${appCssModules?.digest ?? '// n/a'}</code>
+    </pre>
+    <pre>
+      <code>${h?.digest ?? '// n/a'}</code>
+    </pre>
+    <pre>
+      <code>${h?.css ?? '// n/a'}</code>
+    </pre>
+  </>
+);
