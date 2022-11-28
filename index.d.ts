c798d7b45f0d8f3d9cfe0071e8e5d0bfeb67f71c
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2022-11-29 09:17:13 +1100

    skip generating build ID when inject is false

--- index.d.ts
+++ index.d.ts
@@ -73,7 +73,7 @@ declare interface PluginOptions {
 }
 
 declare interface BuildContext {
-  buildId: string;
+  buildId?: string;
   buildRoot: string;
   packageRoot?: string;
   packageVersion: string;
