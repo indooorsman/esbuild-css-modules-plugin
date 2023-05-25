e1ce890ddacf8eb543295586fb38bb50f44817ff
indooorsman <indooorsman@gmail.com>
2023-05-25 14:30:02 +0800

    dev: replace all matched .css to .js for cjs

--- index.cjs
+++ index.cjs
@@ -1,21 +1,7 @@
 "use strict";
 var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
-};
-var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
-    for (let key of __getOwnPropNames(from))
-      if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-  }
-  return to;
-};
+
 var __publicField = (obj, key, value) => {
   __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
   return value;
@@ -568,7 +554,7 @@ var CssModulesPlugin = (_options) => {
                 let newContent = content;
                 Object.entries(places).forEach(([f, t]) => {
                   log(`fix import path in ${js}: ${f} ===> ${t}`);
-                  newContent = newContent.replace(f, t);
+                  newContent = newContent.replaceAll(f, t);
                 });
                 return newContent;
               }).then((nc) => {
